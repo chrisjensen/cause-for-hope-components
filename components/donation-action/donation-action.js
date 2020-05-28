ab28c8987da38bc2036b2cc00c384f3c636c09d8
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-05-29 09:38:38 +1200

    Add progress bar, use real profile

--- components/donation-action/donation-action.js
+++ components/donation-action/donation-action.js
@@ -1,25 +1,38 @@
 (RaiselyComponents, React) => {
-	const { Button } = RaiselyComponents.Atoms;
+	const { Button, ProgressBar } = RaiselyComponents.Atoms;
 	const { DonationForm } = RaiselyComponents.Molecules;
+	const { api } = RaiselyComponents;
+	const { getData } = api;
+
+	const profilePath = 'original-power';
 
 	return class DonationAction extends React.Component {
-		state = {};
+		state = { profile: { name: 'Original Power' }};
+
+		componentDidMount() {
+			this.load();
+		}
+
+		async load() {
+			try {
+				const profile = await getData(api.profiles.get({ id: profilePath }));
+				this.setState({ profile });
+			} catch (e) {
+				console.error(e);
+			}
+		}
 
 		donate = () => this.setState({ showDonate: true });
 
 		render() {
 			const { props } = this;
-			const { showDonate } = this.state;
-			const profile = {
-				name: 'ACOSS - Australian Council of Social Services',
-				description: `ACOSS are the peak body of Australian advocates for those living with poverty, disadvantage or inequality. People who especially need a voice now.`,
-			};
+			const { profile, showDonate } = this.state;
 
 			if (showDonate) {
 				return (
 					<div className="donation-action">
 						<DonationForm
-							profileUuid={null}
+							profileUuid={profile.uuid}
 							title={`Donate to support ${profile.name}`}
 							recurring={true}
 							integrations={props.integrations}
@@ -33,13 +46,20 @@
 					<div className="action-list__item">
 						<div className="action-list__body">
 							<h3>Donate to support {profile.name}</h3>
-							<p><strong>The organisations the advocate for people and planet depend on the generosity of donors to fund their advocacy.</strong></p>
+							<p><strong>The organisations that advocate for people and planet depend on the generosity of donors to fund their advocacy.</strong></p>
 							<p>{profile.description}</p>
 						</div>
 						<div className="action-list__button-wrapper">
 							<Button onClick={this.donate} theme="secondary">Donate</Button>
 						</div>
 					</div>
+					<ProgressBar
+						profile={profile}
+						statPosition="middle"
+						showTotal={false}
+						showGoal={false}
+						style="rounded"
+					/>
 				</div>
 			);
 		}
