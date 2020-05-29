49eaf15a68e1941b8e236cff4aeb068952180954
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-05-30 02:23:03 +1200

    Add service worker registration and checking for edge cases

--- components/notify-subscribe/notify-subscribe.js
+++ components/notify-subscribe/notify-subscribe.js
@@ -1,16 +1,55 @@
+/**
+ * Push notification subscribe component
+ * Thanks to @Spyna for the tutorial on this
+ * https://github.com/Spyna/push-notification-demo/
+ */
 (RaiselyComponents, React) => {
-	const { useState, useEffect } = React;
+	const { useEffect, useState } = React;
+	const { Spinner } = RaiselyComponents;
 
-	const { api } = RaiselyComponets;
+	const { api } = RaiselyComponents;
 	const { Button } = RaiselyComponents.Atoms;
 	const { getData } = api;
 
 	const isNotifySupported = () => "serviceWorker" in navigator && "PushManager" in window;
 
 	return function NotifySubscribe({ onSubscribe }) {
+		const [loading, setLoading] = useState(false);
 		const [grant, setGrant] = useState(null);
 		const [error, setError] = useState(null);
-		const [subscribed, setSubscribed] = useState(null);
+		const [isSubscribing, setSubscribing] = useState();
+		const [isSubscribed, setSubscribed] = useState(null);
+
+		useEffect(() => {
+			if (isNotifySupported()) {
+				setLoading(true);
+				setError(false);
+				navigator.serviceWorker.register("/sw.js")
+					.then(() => {
+					})
+					.catch(e => {
+						setError(e);
+						console.error(e);
+						setLoading(false);
+					});
+			}
+		}, []);
+
+		useEffect(() => {
+			const getExixtingSubscription = async () => {
+				try {
+					const serviceWorker = await navigator.serviceWorker.ready;
+					const existingSubscription = await serviceWorker.pushManager.getSubscription();
+					setLoading(false);
+					setSubscribed(!!existingSubscription);
+				} catch (e) {
+					setError(e);
+					console.error(e);
+					setLoading(false)
+				}
+			};
+			getExixtingSubscription();
+		}, []);
 
 		async function doSubscribe() {
 			const consent = await Notification.requestPermission();
@@ -25,22 +64,49 @@
 					userVisibleOnly: true,
 					applicationServerKey: pushServerPublicKey,
 				});
-				await getData(api.users.update({ private: { subscription: subscription.endpoint }}))
+				setSubscribing(true);
+				await getData(api.users.update({ private: { subscription: JSON.stringify(subscription) }}))
 				setSubscribed(true);
+				// Notify calling component that subscription is complete
 				if (onSubscribe) onSubscribe();
 			} catch (err) {
+				setSubscribing(false);
 				console.error(err);
 				setError(err);
+			} finally {
+				setSubscribing(false);
 			}
+		}
+
+		if (loading) {
+			return <Spinner />;
+		}
+
+		if (isSubscribed) {
+			return (
+				<div className="subscribe">
+					<p>{`You're`} subscribed!</p>
+				</div>
+			)
+		}
+
+		if (error) {
+			return (
+				<div className="subscribe">
+					<p className="error">Error subscribing</p>
+				</div>
+			)
 		}
 
 		return (
 			<div className="subscribe">
-				<p>{`Don't`} miss out! Get notified when {`there's`} more you can do</p>
+				<p>{`Don't`} miss out! Get notified when {`there's`} more actions to take</p>
 				{grant === 'denied' && (
 					<p>You need to grant permission for us to send you notifications</p>
 				)}
-				<Button onClick="doSubscribe">Notify Me</Button>
+				<Button onClick={doSubscribe} disabled={isSubscribing}>
+					{isSubscribing && <Spinner />} Notify Me
+				</Button>
 			</div>
 		);
 	};
